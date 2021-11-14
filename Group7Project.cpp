25551da1d175b20410aa9e5f871d0c83e27e7145
SidK <76796780+IsolatedCorvus@users.noreply.github.com>
2021-11-13 19:31:28 -0800

    Update Group7Project.cpp
    Made sure all the programs work correctly while displaying the time of execution.
    From Insertion Sort - Bubble Sort works
    Need Quicksort and Heapsort with their respective function calls.

--- Group7Project.cpp
+++ Group7Project.cpp
@@ -2,7 +2,8 @@
 #include<cstdlib>
 #include<vector>
 #include<ctime>
-#include"groups_code.h";
+#include<unordered_set>
+#include"groups_code.h"
 using namespace std;
 
 
@@ -11,7 +12,7 @@ using namespace std;
 #define Max2 10000
 #define Max3 70000
 #define CountMax1 100
-#define countMax2 10000
+#define CountMax2 10000
 #define CountMax3 70000
 #define RadMax1 100
 #define RadMax2 10000
@@ -19,7 +20,7 @@ using namespace std;
 #define MergeMax1 100
 #define MergeMax2 10000
 #define MergeMax3 60000
-
+#define BruteMax 80000
 
 
 
@@ -29,7 +30,8 @@ using namespace std;
 
 int main()
 {
-	/*-InsertionSort-*/
+	
+	//-InsertionSort-
 	clock_t insertionTime;
 	int arrSorted1[Max1], arrHalf1[Max1], arrReversed1[Max1], arrSorted2[Max2], arrHalf2[Max2], arrReversed2[Max2], arrSorted3[Max3], arrHalf3[Max3], arrReversed3[Max3]; //All different size of list.
 	cout << "-Insertion sort-" << endl << endl;
@@ -60,10 +62,10 @@ int main()
 	insertionTime = clock() - insertionTime; //End time
 	cout << "Execution time of half sorted list of 100 elements: " << (float)insertionTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
-	int r = 0; //For reverse
+	int ra = 0; //For reverse
 	for (int i = Max1; i > 0; i--)	// Reversed Array
 	{
-		arrReversed1[r++] = i; //Place the values in reverse.
+		arrReversed1[ra++] = i; //Place the values in reverse.
 	}
 	int reversesize1 = sizeof(arrReversed1) / sizeof(arrReversed1[0]); //Getting the size
 	insertionTime = clock(); //Start time
@@ -99,10 +101,10 @@ int main()
 	insertionTime = clock() - insertionTime; //End time
 	cout << "Execution time of half sorted list of 10000 elements: " << (float)insertionTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
-	int b = 0;
+	int ba = 0;
 	for (int i = Max2; i > 0; i--)	// Reversed Array
 	{
-		arrReversed2[b++] = i;
+		arrReversed2[ba++] = i;
 	}
 	int reversesize2 = sizeof(arrReversed2) / sizeof(arrReversed2[0]); //Getting the size of the list.
 	insertionTime = clock(); //Start time
@@ -138,18 +140,18 @@ int main()
 	insertionTime = clock() - insertionTime; //End time
 	cout << "Execution time of half sorted list of 700000 elements: " << (float)insertionTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
-	int c = 0;
+	int ca = 0;
 	for (int i = Max3; i > 0; i--)	// Reversed Array
 	{
-		arrReversed3[c++] = i;
+		arrReversed3[ca++] = i;
 	}
 	int reversesize3 = sizeof(arrReversed3) / sizeof(arrReversed3[0]); //Getting the 
 	insertionTime = clock();
 	BBinsertion(arrReversed3, reversesize3);
 	insertionTime = clock() - insertionTime;
-	cout << "Execution time of reverse sorted list of 70000 elements: " << (float)insertionTime / CLOCKS_PER_SEC << endl;
+	cout << "Execution time of reverse sorted list of 70000 elements: " << (float)insertionTime / CLOCKS_PER_SEC << endl << endl;
 
-	/*-SelectionSort-*/
+	//-SelectionSort-
 	clock_t selectionTime;
 	int SelSorted1[Max1], SelHalf1[Max1], SelReversed1[Max1], SelSorted2[Max2], SelHalf2[Max2], SelReversed2[Max2], SelSorted3[Max3], SelHalf3[Max3], SelReversed3[Max3];
 	cout << "-Selection sort- " << endl << endl;
@@ -174,16 +176,17 @@ int main()
 			SelHalf1[i] = i;
 		}
 	}
+	
 	int Selhalfsize1 = sizeof(SelHalf1) / sizeof(SelHalf1[0]);
 	selectionTime = clock();
 	BBselectionsorting(SelHalf1, Selhalfsize1);
 	selectionTime = clock() - selectionTime;
 	cout << "Execution time of half sorted list of 100 elements: " << (float)selectionTime / CLOCKS_PER_SEC << endl;
 
-	int r = 0;
+	int rb = 0;
 	for (int i = Max1; i > 0; i--)		// Reversed Array
 	{
-		SelReversed1[r++] = i;
+		SelReversed1[rb++] = i;
 	}
 	int Selreversesize1 = sizeof(SelReversed1) / sizeof(SelReversed1[0]);
 	selectionTime = clock();
@@ -219,10 +222,10 @@ int main()
 	selectionTime = clock() - selectionTime;
 	cout << "Execution time of half sorted list of 10000 elements: " << (float)selectionTime / CLOCKS_PER_SEC << endl;
 
-	int b = 0;
+	int bb = 0;
 	for (int i = Max2; i > 0; i--)		// Reversed Array
 	{
-		SelReversed2[b++] = i;
+		SelReversed2[bb++] = i;
 	}
 	int Selreversesize2 = sizeof(SelReversed2) / sizeof(SelReversed2[0]);
 	selectionTime = clock();
@@ -258,10 +261,10 @@ int main()
 	selectionTime = clock() - selectionTime;
 	cout << "Execution time of half sorted list of 70000 elements: " << (float)selectionTime / CLOCKS_PER_SEC << endl;
 
-	int c = 0;
+	int cb = 0;
 	for (int i = Max3; i > 0; i--)		// Reversed Array
 	{
-		SelReversed3[c++] = i;
+		SelReversed3[cb++] = i;
 	}
 	int Selreversesize3 = sizeof(SelReversed3) / sizeof(SelReversed3[0]);
 	selectionTime = clock();
@@ -269,7 +272,7 @@ int main()
 	selectionTime = clock() - selectionTime;
 	cout << "Execution time of reverse sorted list of 70000: " << (float)selectionTime / CLOCKS_PER_SEC << endl;
 
-	/*-CountingSort-*/
+	//-CountingSort-
 	clock_t CountTime;
 	int count0B[CountMax1];
 	int count0C[CountMax1 + 1];
@@ -364,7 +367,7 @@ int main()
 	CountTime = clock(); //Start time
 	BBCountingSort(CountSorted3, countsize3, count2B, count2C); //Calling the counting sort function to sort.
 	CountTime = clock() - CountTime; //End time
-	cout << "Execution time of already sorted list of 1 million elements: " << (float)CountTime / CLOCKS_PER_SEC << endl;
+	cout << "Execution time of already sorted list of 70000 elements: " << (float)CountTime / CLOCKS_PER_SEC << endl;
 	for (int i = 0; i < CountMax3; i++)
 	{
 		if (i % 6 == 0 && i != 0)
@@ -380,7 +383,7 @@ int main()
 	CountTime = clock(); //Start time
 	BBCountingSort(CountHalf3, counthalfsize3, count2B, count2C); //Calling the counting sort function to sort.
 	CountTime = clock() - CountTime; //End time
-	cout << "Execution time of half sorted list of 1 million elements: " << (float)CountTime / CLOCKS_PER_SEC << endl;
+	cout << "Execution time of half sorted list of 70000 elements: " << (float)CountTime / CLOCKS_PER_SEC << endl;
 
 	int ccount = 0;
 	for (int i = CountMax3; i > 0; i--)
@@ -391,9 +394,12 @@ int main()
 	CountTime = clock(); //Start time
 	BBCountingSort(CountReversed3, countReversesize3, count2B, count2C); //Calling the counting sort function to sort.
 	CountTime = clock() - CountTime; //End time
-	cout << "Execution time of reverse sorted list of 1 million elements: " << (float)CountTime / CLOCKS_PER_SEC << endl << endl;
+	cout << "Execution time of reverse sorted list of 70000 elements: " << (float)CountTime / CLOCKS_PER_SEC << endl << endl;
 
-	/*-Radix-*/
+
+
+
+	//-Radix-//
 	clock_t radixTime;
 	int RadixSorted1[RadMax1], RadixHalf1[RadMax1], RadixReversed1[RadMax1], RadixSorted2[RadMax2], RadixHalf2[RadMax2], RadixReversed2[RadMax2], RadixSorted3[RadMax3], RadixHalf3[RadMax3], RadixReversed3[RadMax3];
 	cout << "-Radix sort-" << endl << endl;
@@ -512,10 +518,7 @@ int main()
 	BBRadixSort(RadixReversed3, reversesize3);
 	radixTime = clock() - radixTime;
 	cout << "Execution time of reverse sorted list of 45000 elements: " << (float)radixTime / CLOCKS_PER_SEC << endl;
-
-
-
-
+	
 	/////////////////////////////////////// Merge Sort
 
 	clock_t MergeTime;
@@ -553,13 +556,13 @@ int main()
 	cout << "Execution time of half sorted list of 100 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl;
 
 	///////////////////////////////////////
-	int r = 0; //For reverse
+	int rd = 0; //For reverse
 	for (int i = MergeMax1; i > 0; i--)	// Reversed Array
 	{
-		MergeReversed1[r++] = i; //Place the values in reverse.
+		MergeReversed1[rd++] = i; //Place the values in reverse.
 	}
 
-	int reversesize1 = sizeof(arrReversed1) / sizeof(arrReversed1[0]);
+	int reversesize1 = sizeof(MergeReversed1) / sizeof(MergeReversed1[0]);
 	MergeTime = clock();
 	SKMergeSort(MergeReversed1, 0, (reversesize1 - 1));
 	MergeTime = clock() - MergeTime;
@@ -570,10 +573,10 @@ int main()
 	{
 		MergeSorted2[i] = i; //Having the elements be place in the array in order already
 	}
-	int sorted2 = sizeof(MergeSorted2) / sizeof(MergeSorted2[0]); //Getting the size of the list
+	int sorted2a = sizeof(MergeSorted2) / sizeof(MergeSorted2[0]); //Getting the size of the list
 
 	MergeTime = clock();
-	SKMergeSort(MergeSorted2, 0, (sorted2 - 1));
+	SKMergeSort(MergeSorted2, 0, (sorted2a - 1));
 	MergeTime = clock() - MergeTime;
 	cout << "Execution time of already sorted list of 10000 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
@@ -597,10 +600,10 @@ int main()
 	cout << "Execution time of half sorted list of 10000 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl;
 
 	///////////////////////////////////////
-	int r = 0; //For reverse
+	int re = 0; //For reverse
 	for (int i = MergeMax2; i > 0; i--)	// Reversed Array
 	{
-		MergeReversed2[r++] = i; //Place the values in reverse.
+		MergeReversed2[re++] = i; //Place the values in reverse.
 	}
 
 	int reversesize2 = sizeof(MergeReversed2) / sizeof(MergeReversed2[0]);
@@ -614,10 +617,10 @@ int main()
 	{
 		MergeSorted3[i] = i; //Having the elements be place in the array in order already
 	}
-	int sorted2 = sizeof(MergeSorted3) / sizeof(MergeSorted3[0]); //Getting the size of the list
+	int sorted2b = sizeof(MergeSorted3) / sizeof(MergeSorted3[0]); //Getting the size of the list
 
 	MergeTime = clock();
-	SKMergeSort(MergeSorted3, 0, (sorted3 - 1));
+	SKMergeSort(MergeSorted3, 0, (sorted2b - 1));
 	MergeTime = clock() - MergeTime;
 	cout << "Execution time of already sorted list of 60000 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
@@ -641,10 +644,10 @@ int main()
 	cout << "Execution time of half sorted list of 60000 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl;
 
 	///////////////////////////////////////
-	int r = 0; //For reverse
+	int rf = 0; //For reverse
 	for (int i = MergeMax3; i > 0; i--)	// Reversed Array
 	{
-		MergeReversed3[r++] = i; //Place the values in reverse.
+		MergeReversed3[rf++] = i; //Place the values in reverse.
 	}
 
 	int reversesize3 = sizeof(MergeReversed3) / sizeof(MergeReversed3[0]);
@@ -653,26 +656,25 @@ int main()
 	MergeTime = clock() - MergeTime;
 	cout << "Execution time of reverse sorted list of 60000 elements: " << (float)MergeTime / CLOCKS_PER_SEC << endl << endl << endl; //Getting the execution time.
 
-
-
 	/////////////////////////////////////// Bubble Sort
 
 	clock_t BubbleTime;
+	int arrSorted1[Max1], arrHalf1[Max1], arrReversed1[Max1], arrSorted2[Max2], arrHalf2[Max2], arrReversed2[Max2], arrSorted3[Max3], arrHalf3[Max3], arrReversed3[Max3]; //All different size of list.
 
 	///////////////////////////////////
-	for (int i = 0; i < Max3; i++)	// Sorted Array
+	for (int i = 0; i < Max1; i++)	// Sorted Array
 	{
 		arrSorted1[i] = i; //Having the elements be place in the array in order already
 	}
-	int sorted1 = sizeof(arrSorted1) / sizeof(arrSorted1[0]); //Getting the size of the list
+	int sorted1c = sizeof(arrSorted1) / sizeof(arrSorted1[0]); //Getting the size of the list
 
 	BubbleTime = clock();
-	SKbubbleSort(arrSorted1, sorted1);
+	SKbubbleSort(arrSorted1, sorted1c);
 	BubbleTime = clock() - BubbleTime;
 	cout << "Execution time of already sorted list of 100 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
 
 	///////////////////////////////////
-	for (int i = 0; i < Max3; i++)	// Half Sorted Array
+	for (int i = 0; i < Max1; i++)	// Half Sorted Array
 	{
 		if (i % 6 == 0 && i != 0)
 		{
@@ -684,27 +686,167 @@ int main()
 		}
 	}
 
-	int halfsize1 = sizeof(arrHalf1) / sizeof(arrHalf1[0]);
+	int halfsize1c = sizeof(arrHalf1) / sizeof(arrHalf1[0]);
 	BubbleTime = clock();
-	SKbubbleSort(arrHalf1, halfsize1);
+	SKbubbleSort(arrHalf1, halfsize1c);
 	BubbleTime = clock() - BubbleTime;
 	cout << "Execution time of half sorted list of 100 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl;
 
 	////////////////////////////////////
-	int r = 0; //For reverse
-	for (int i = Max3; i > 0; i--)	// Reversed Array
+	int rg = 0; //For reverse
+	for (int i = Max1; i > 0; i--)	// Reversed Array
 	{
-		arrReversed1[r++] = i; //Place the values in reverse.
+		arrReversed1[rg++] = i; //Place the values in reverse.
 	}
 
-	int reversesize1 = sizeof(arrReversed1) / sizeof(arrReversed1[0]);
+	int reversesize1y = sizeof(arrReversed1) / sizeof(arrReversed1[0]);
 	BubbleTime = clock();
-	SKbubbleSort(arrReversed1, reversesize1);
+	SKbubbleSort(arrReversed1, reversesize1y);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of reverse sorted list of 100 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl << endl << endl; //Getting the execution time.
+	
+		///////////////////////////////////
+	for (int i = 0; i < Max2; i++)	// Sorted Array
+	{
+		arrSorted2[i] = i; //Having the elements be place in the array in order already
+	}
+	int sorted2d = sizeof(arrSorted2) / sizeof(arrSorted2[0]); //Getting the size of the list
+
+	BubbleTime = clock();
+	SKbubbleSort(arrSorted2, sorted2d);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of already sorted list of 10000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
+
+	///////////////////////////////////
+	for (int i = 0; i < Max2; i++)	// Half Sorted Array
+	{
+		if (i % 6 == 0 && i != 0)
+		{
+			arrHalf2[i] = rand() % 100;		// Random number inserted every 6th element.
+		}
+		else
+		{
+			arrHalf2[i] = i;
+		}
+	}
+
+	int halfsize2d = sizeof(arrHalf2) / sizeof(arrHalf2[0]);
+	BubbleTime = clock();
+	SKbubbleSort(arrHalf2, halfsize2d);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of half sorted list of 10000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl;
+
+	////////////////////////////////////
+	int rh = 0; //For reverse
+	for (int i = Max2; i > 0; i--)	// Reversed Array
+	{
+		arrReversed2[rh++] = i; //Place the values in reverse.
+	}
+
+	int reversesize2t = sizeof(arrReversed2) / sizeof(arrReversed2[0]);
+	BubbleTime = clock();
+	SKbubbleSort(arrReversed2, reversesize2t);
 	BubbleTime = clock() - BubbleTime;
 	cout << "Execution time of reverse sorted list of 10000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl << endl << endl; //Getting the execution time.
 
+		///////////////////////////////////
+	for (int i = 0; i < Max3; i++)	// Sorted Array
+	{
+		arrSorted3[i] = i; //Having the elements be place in the array in order already
+	}
+	int sorted3e = sizeof(arrSorted3) / sizeof(arrSorted3[0]); //Getting the size of the list
 
+	BubbleTime = clock();
+	SKbubbleSort(arrSorted3, sorted3e);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of already sorted list of 70000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl; //Getting the execution time.
+
+	///////////////////////////////////
+	for (int i = 0; i < Max3; i++)	// Half Sorted Array
+	{
+		if (i % 6 == 0 && i != 0)
+		{
+			arrHalf3[i] = rand() % 100;		// Random number inserted every 6th element.
+		}
+		else
+		{
+			arrHalf3[i] = i;
+		}
+	}
+
+	int halfsize3e = sizeof(arrHalf3) / sizeof(arrHalf3[0]);
+	BubbleTime = clock();
+	SKbubbleSort(arrHalf3, halfsize3e);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of half sorted list of 70000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl;
+
+	////////////////////////////////////
+	int ri = 0; //For reverse
+	for (int i = Max3; i > 0; i--)	// Reversed Array
+	{
+		arrReversed3[ri++] = i; //Place the values in reverse.
+	}
+
+	int reversesize3w = sizeof(arrReversed3) / sizeof(arrReversed3[0]);
+	BubbleTime = clock();
+	SKbubbleSort(arrReversed3, reversesize3w);
+	BubbleTime = clock() - BubbleTime;
+	cout << "Execution time of reverse sorted list of 70000 elements: " << (float)BubbleTime / CLOCKS_PER_SEC << endl << endl << endl; //Getting the execution time.
+
+	////////////////////////////////////
+	clock_t bruteTime;
+	int xvalue; //The sum
+	cout << endl << "In brute/inefficent method of sum, it will go through the 80,000 listed numbers to find how many pairs there is for the sum. "; 
+	int BruteArray[BruteMax]; //Max size of the list is 80,000 which is going through.
+	cout << endl << "Enter a value for the sum: ";
+	cin >> xvalue; //User input a value for the sum that will be looking for.
+	cout << endl;
+	for (int i = 0; i < BruteMax; i++) //Making the list with the numbers
+	{
+		BruteArray[i] = i;
+	}
+	int bruteArraysize = sizeof(BruteArray) / sizeof(BruteArray[0]); //Getting the size of the array.
+	cout << "In sorted already list:" << endl;
+	bruteTime = clock(); //Start time
+	bruteSum(BruteArray, bruteArraysize, xvalue); //Calling the function
+	bruteTime = clock() - bruteTime; //End time
+	cout << "Execution time of bruteSum time: " << (float)bruteTime / CLOCKS_PER_SEC << endl << endl << endl; //Output the time to execute.
+
+	//Vector to hold the array
+    vector<int> list;
+    //int to hold the user selected size of the array
+    int size = 0;
+    //int to hold the sum integer
+    int x = 0;
+    //user prompt
+    cout << "Please enter a size that you would like for a randomly generated array.\n";
+    //array size input from user
+    cin >> size;
+    //for loop that generates random array of size that the user enetered
+    for (int i = 0; i < size; i++) {
+        //Pushes the generated elements in the vector named list
+        list.push_back(rand() % size);
+    }
+    //Outputs generated array - uncommment to see it 
+   /* 
+   cout << "Generated Array:\n";
+    //for loop that prints the generated array
+    for (int i = 0; i < size; i++) {
+        cout << list.at(i) << " ";
+    }
+    */
+    //Endline
+    cout << endl;
+    //User input for integer x
+        cout << "Please enter the integer X you would like to use\n";
+        //takes the input
+        cin >> x;
+        //start the clock timer
+        clock_t StartTime = clock();
+        //calls the sum_check function that hold the algorithm
+        sum_check(list, x);
+        //print the time
+        printf("\nTime: %.2fs\n", (float)(clock() - StartTime) / CLOCKS_PER_SEC);
 
 	return 0;
 }
-
